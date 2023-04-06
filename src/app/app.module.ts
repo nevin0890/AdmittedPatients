d907ace9351f2dfaa5960c6dcdbd9e21b71e8189
Nevin Joy <Nevin@NTP-LAP-1204.chn.nestgroup.net>
2023-04-06 15:31:27 +0530

    MPV UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewPatientsComponent } from './view-patients/view-patients.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const routes: Routes = [
+  { path: '', component: ViewPatientsComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewPatientsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
